7bad6445585eef565c140b789ae7afc0ddcaa102
llach <llach@techfak.uni-bielefeld.de>
2018-04-20 15:23:00 +0200

    deleted interface build instructions

--- posture_execution/setup.py
+++ posture_execution/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-     packages=['posture_execution', 'posture_execution.interfaces'],
+     packages=['posture_execution'],
      package_dir={'': 'src'}
 )
 
